b240f821d454568a3da43d07644df3f4e8bdae78
oleg.gulitsky <oleg.gulitsky@gmail.com>
2024-09-12 15:15:52 +0300

    Initial changes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+export default function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <Text>Comments RN App</Text>
+    </SafeAreaView>
+  );
+}
